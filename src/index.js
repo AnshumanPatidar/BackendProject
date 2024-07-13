38595f23592198c6f981abe1108b0cf7fad71d8d
AnshumanPatidar <anshumanpatidar355@gmail.com>
2024-07-13 12:04:38 +0530

    all setup configured and checked if route is working

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 
 // import mongoose from "mongoose"
 // import { DB_NAME } from "./constants"
-// // this approach is good but even better approach is to write your code in db folder 
-// // the reason we make db folder is to have database related code in one place 
+// // this approach is good but even better approach is to write your code in db folder
+// // the reason we make db folder is to have database related code in one place
 
 // import express from "express"
 // const app = express()
@@ -13,17 +13,15 @@
 //    try {
 //     await  mongoose.connect(`${process.env.MONGO_DB_URI}/   ${DB_NAME}`)
 //     // if database is connected succ.. also check if app is working fine or not
-//     app.on("error" , (error) => { 
+//     app.on("error" , (error) => {
 //       console.error("ERR" , error)
 //       throw error
 //     })
-
 
 //     // if everything is fine then start listening on the decided port
 //     app.listen(process.env.PORT , () => {
 //       console.log(`App is listening on port ${process.env.PORT}`)
 //     })
-
 
 //    // if database connection is not made than throw error
 //    } catch (error) {
@@ -33,26 +31,28 @@
 
 //   })()
 
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
-import { app } from './app.js'
-
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: './env'
-})
+  path: "./.env",
+});
 
+connectDB()
+  .then(() => {
+    // app.on = Listening for Errors
+    //You can listen for the error event to handle unexpected errors that might occur in your application.
+    // Listen for application-level errors
+    app.on("error", (error) => {
+      console.error("Application Error:", error);
+      throw error;
+    });
 
-connectDB().then(() => {
-  // app.on = Listening for Errors
-//You can listen for the error event to handle unexpected errors that might occur in your application.
-  app.on('error' , (error) => {
-    console.log("ERR: " , error)
-    throw error
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at ${process.env.PORT}`);
+    });
   })
-  app.listen(process.env.PORT || 4000 , () => {
-    console.log(`server is runing at ${process.env.PORT}`)
-  })
-}).catch((err) => {
-  console.log('mongo DB connection failed !! ' , err)
-})
+  .catch((err) => {
+    console.log("mongo DB connection failed !! ", err);
+  });
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,18 @@
 const asyncHandler = (requestHandler) => {
-    return (req ,res , next ) => {
-       Promise.resolve(requestHandler(req , res , next))
-       .catch((error) => next(error))
-    }
-}
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
 
-export {asyncHandler}
-
+export { asyncHandler };
 
 // const asyncHandler = ( ) => {}
-  // const asyncHandler = (func) => ( ) => {}
-     // const asyncHandler = (func) => async () => {}
+// const asyncHandler = (func) => ( ) => {}
+// const asyncHandler = (func) => async () => {}
 
-
-// THIS IS A WRAPPER FUNCTION USING TRY AND CATCH BUT WE ARE NOT GOING TO USE IT 
+// THIS IS A WRAPPER FUNCTION USING TRY AND CATCH BUT WE ARE NOT GOING TO USE IT
 // ACTUALY WHAT WE ARE DOING HERE IT WE ARE WRAPPING OUR ASYNCRONOUS FUNCTION OR PROCESSES
 // SO IF WE EVER GET A ASYNCRONOUS FUNCTION SO WE WILL PASS THE FUNCTION TO HANDLE ITS PROMISE
 // NHI AAYE SMJH ME TOH VIDEO DELK LENA 8 TH NUMBER K 😎
@@ -21,10 +20,9 @@ export {asyncHandler}
 
 // THIS IS A HIGHER ORDER FUNCTION (FUNCTION THAT TAKE FUNTION AS A ARGUMENT AND TREAT THEM AS VARIABLE)
 
-
 // const asyncHandler = (func) => async (req , res , next ) => {
 //          try {
-//             await func(req ,res , next) 
+//             await func(req ,res , next)
 //          } catch (error) {
 //              res.status(error.code || 500).json({
 //               success : false,
