631d9c87a3cae87e82277280e67778fe2b501b13
AnshumanPatidar <anshumanpatidar355@gmail.com>
2024-07-13 15:43:58 +0530

    add user register controller and bug fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,96 +1,97 @@
-import mongoose , {Schema}from 'mongoose'
-import  jwt from 'jsonwebtoken';
-import bcrypt from  'bcrypt';
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-   username : {
-      type : String,
-      required : true,
-      unique : true,
-      lowercase : true,
-      trim : true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
       // if you know that this field will be searched regularly
       // then put its index : true to get it inside searching of data base
-      // nhi aaye smjh me toh video dekh lena 
+      // nhi aaye smjh me toh video dekh lena
       // hr kisi ko index mt kr dena nhi toh performance bigaad jaaye gi
-      index : true,
-   },
-    email : {
-      type : String,
-      required : true,
-      unique : true,
-      lowercase : true,
-      trim : true,
-  
- },
-    fullname : {
-      type : String,
-      required : true,
-      trim : true,
-      index : true
- },
- avatar : {
-     type : String, // cloudnary url use krenge
-     required : true
- },
- coverImage : {
-     type : String , // cloudnary url use krenge
- },
- watchHistory : [
-   {
-    type : Schema.Types.ObjectId,
-    ref: "Video"
-   }
- ],
- password : {
-  type : String,
-  required : [true , 'Password is required']
- }
-} , {timestamps : true});
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudnary url use krenge
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudnary url use krenge
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-
-userSchema.pre("save" , async function (next) {
-    if(!this.isModified("password")){
-      return next()
-    }
-    this.password = bcrypt.hash(this.password , 10);
-    next();
-})
-
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return  await bcrypt.compare(password  , this.password)
-}
-
-
-// jwt token
-userSchema.methods.generateAccessToken = function () {
-    return jwt.sign(
-        {
-            _id: this._id,
-            email:this.email,
-            username :  this.username,
-            fullname : this.fullname
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-          expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-        }
-     )
-}
+  return await bcrypt.compare(password, this.password);
+};
 
 // jwt token
 userSchema.methods.generateAccessToken = function () {
-      return jwt.sign(
-        {
-            _id: this._id,
-        },
-        process.env.REFRESS_TOKEN_SECRET,
-        {
-          expiresIn: process.env.REFRESS_TOKEN_EXPIRY
-        }
-    )
-}
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
+// jwt token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
-export const User = mongoose.model("User" , userSchema)
+export const User = mongoose.model("User", userSchema);
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,11 +1,13 @@
 class ApiResponse {
-  constructor(statusCode , data , message = "Success"){
-    this.statusCode = statusCode
-    this.data = data
-    this.message = message
-    this.success = statusCode < 400 
-    // statusCode less than 400 rakh rhe h 
-    // Api Response me agr dikkat aati h toh 400 ke niche status code rkhenge 
-    // kyu ki apn ne 500 wala Api error ke liye select kr liya h 
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+    // statusCode less than 400 rakh rhe h
+    // Api Response me agr dikkat aati h toh 400 ke niche status code rkhenge
+    // kyu ki apn ne 500 wala Api error ke liye select kr liya h
   }
 }
+
+export { ApiResponse };
